de6c07805a69522e127488402a9f2c39d67987b4
namtrader15 <namntn158@gmail.com>
2024-09-12 01:14:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def home():
     return "Hello, this is your Flask app on Railway!"
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))  # Lấy giá trị PORT từ biến môi trường, mặc định là 5000 nếu không có
-    app.run(host='0.0.0.0', port=port)
+# python web server
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
